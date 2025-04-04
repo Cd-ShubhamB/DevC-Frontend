0752c855882b60d890d63d72680848e865d2cf51
Shubham <shubhambaikar2@gmail.com>
2025-04-04 19:16:32 +0530

    Fix import path for axios

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
-import api from "../api/axios";
+import api from "./api/axios";
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AuthProvider } from "./context/AuthContext";
 import App from './App.jsx'
-import api from "../api/axios";
+import api from "./api/axios";
 
 
 createRoot(document.getElementById('root')).render(
